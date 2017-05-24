4a0c66a8c1a9ebd83ec1ac8a5e0603fd54ae37c5
Bill Hefty <billhefty@gmail.com>
2017-05-23 22:11:53 -0500

    add contact me section

---
+++ components/contactSpacer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactSpacer = () => {
+    return (
+        <section id='contact-spacer'>
+            <h1 className='spacer-head'>CONTACT ME</h1>
+        </section>
+    );
+};
+
+export default ContactSpacer;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Layout from '../components/layout'
 import About from '../components/about'
 import ProjectSpacer from '../components/projectSpacer'
 import ProjectsSection from '../components/projectsSection'
+import ContactSpacer from '../components/contactSpacer'
+import Contact from '../components/contact'
 
 export default () => (
     <div>
@@ -10,60 +12,8 @@ export default () => (
             <About />
             <ProjectSpacer />
             <ProjectsSection />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <ContactSpacer />
+            <Contact />
         </Layout>
     </div>
 )
