72d194d82714fb762cc5360a3e1161b11efa7861
Bill Hefty <billhefty@gmail.com>
2017-05-26 16:35:30 -0500

    add projects splash header

---
+++ pages/projects.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import Layout from '../components/layout'
+import Splash from '../components/splash'
+
+export default() => (
+    <Layout
+        title='Bill Hefty | Projects' 
+        splashHeader={(<Splash
+            page='projects'
+            header='projects'
+            subHeader='Checkout some of my recent projects.'
+        />)}
+    >
+        <h1>Hello from projects</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    </Layout>
+)
