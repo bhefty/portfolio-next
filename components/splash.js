5ed43f3d6bad777f719ede4c68f3a3ab2ce50080
Bill Hefty <billhefty@gmail.com>
2017-05-25 21:17:32 -0500

    fix parallax on mobile for splash

--- components/splash.js
+++ components/splash.js
@@ -2,6 +2,7 @@ import SocialStrip from '../components/socialStrip'
 
 export default () => (
     <div className='splash-container'>
+        <div className="splash-background"></div>
         <div className='splash-text'>
             <h1 className='splash-head'>bill hefty</h1>
             <p className='splash-subhead'>Developing engaging and productive web solutions.</p>
