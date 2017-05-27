391f23a7eb6869461560861d0e155e356d0f8002
Bill Hefty <billhefty@gmail.com>
2017-05-27 12:01:22 -0500

    remove http prefix

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'API_URL': prod ? 'http://portfoliowp.x10host.com/wp-json/wp/v2/posts' : 'http://portfoliowp.x10host.com/wp-json/wp/v2/posts'
+  'API_URL': prod ? '//portfoliowp.x10host.com/wp-json/wp/v2/posts' : '//portfoliowp.x10host.com/wp-json/wp/v2/posts'
 }
