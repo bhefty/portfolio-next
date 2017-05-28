506c85d54cb848ace8e07d38d1b438f85ab90a64
Bill Hefty <billhefty@gmail.com>
2017-05-27 23:39:01 -0500

    add getInitialProps to fetch data

--- pages/projects.js
+++ pages/projects.js
@@ -1,16 +1,27 @@
+import React, { Component } from 'react'
 import Layout from '../components/layout'
 import Splash from '../components/splash'
 import ProjectPageList from '../components/projectPageList'
 
-export default() => (
-    <Layout
-        title='Bill Hefty | Projects' 
-        splashHeader={(<Splash
-            page='projects'
-            header='projects'
-            subHeader='Checkout some of my recent projects.'
-        />)}
-    >
-        <ProjectPageList />
-    </Layout>
-)
+export default class extends Component {
+    static async getInitialProps() {
+        const res = await fetch(`http://portfoliowp.x10host.com/wp-json/wp/v2/posts?orderBy=date&order=desc&_embed&categories=4`)
+        const json = await res.json()
+        this.setState({ projects: json })
+    }
+
+    render() {
+        return (
+            <Layout
+                title='Bill Hefty | Projects' 
+                splashHeader={(<Splash
+                    page='projects'
+                    header='projects'
+                    subHeader='Checkout some of my recent projects.'
+                />)}
+            >
+                <ProjectPageList projects={this.props.projects} />
+            </Layout>
+        )
+    }
+}
