9fb96a8a4f0fade4883615cbac4cd97a13441dad
Bill Hefty <billhefty@gmail.com>
2017-05-28 12:58:38 -0500

    correct the absolute path

--- pages/post.js
+++ pages/post.js
@@ -5,18 +5,10 @@ import BlogPost from '../components/blogPost'
 
 export default class extends Component {
     static async getInitialProps(props) {
-        const res = await fetch(`${props.req.protocol}://${props.req.get('Host')}/api/post:${props.query.id}`)
-        const json = res.json()
-        return { post: blogPost }
-        // const res = await fetch(`http://portfoliowp.x10host.com/wp-json/wp/v2/posts/${props.query.id}?_embed`)
-        // const json = await res.json()
-        // const blogPost = {
-        //     date: new Date(json.date).toLocaleDateString(),
-        //     title: json.title.rendered,
-        //     content: json.content.rendered,
-        //     mediaURL: json._embedded['wp:featuredmedia'][0].source_url
-        // }
-        // return { post: blogPost }
+        const protocol = (props.req.secure) ? 'https' : 'http'
+        const res = await fetch(`${protocol}://${props.req.get('Host')}/api/post/${props.query.id}`)
+        const json = await res.json()
+        return { post: json.blogPost }
     }
 
     render() {
