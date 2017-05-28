11119ffc903b376f91366046945a26f5b3bcf57c
Bill Hefty <billhefty@gmail.com>
2017-05-27 22:02:37 -0500

    testing server side call of http

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-
+import React, { Component } from 'react'
 import Layout from '../components/layout'
 import About from '../components/about'
 import ProjectSpacer from '../components/projectSpacer'
@@ -8,16 +8,34 @@ import BlogSection from '../components/blogSection'
 import ContactSpacer from '../components/contactSpacer'
 import Contact from '../components/contact'
 
-export default () => (
-    <div>
-        <Layout>
-            <About />
-            <ProjectSpacer />
-            <ProjectsSection />
-            <BlogSpacer />
-            <BlogSection />
-            <ContactSpacer />
-            <Contact />
-        </Layout>
-    </div>
-)
+export default class extends Component {
+    static async getInitialProps() {
+        const res = await fetch(`http://portfoliowp.x10host.com/wp-json/wp/v2/posts?orderBy=date&per_page=1&_embed&categories=2`)
+        const json = await res.json()
+        const latestPost = {
+            date: new Date(json[0].date).toLocaleDateString(),
+            title: json[0].title.rendered,
+            excerpt: json[0].excerpt.rendered.replace(/<p class=\"link-more\">.*/g, ''),
+            mediaURL: json[0]._embedded['wp:featuredmedia'][0].source_url,
+            id: json[0].id
+        }
+        console.log('server', latestPost)
+        return { post: latestPost }
+    }
+
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <About />
+                    <ProjectSpacer />
+                    <ProjectsSection />
+                    <BlogSpacer />
+                    <BlogSection />
+                    <ContactSpacer />
+                    <Contact />
+                </Layout>
+            </div>
+        )
+    }
+}
