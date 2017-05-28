78842a98a5d787ca0cbb60fb5f2a1580be25734f
Bill Hefty <billhefty@gmail.com>
2017-05-27 23:36:57 -0500

    add getInitialProps to Post page

--- components/blogPost.js
+++ components/blogPost.js
@@ -1,39 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class BlogPost extends Component {
-    async componentDidMount() {
-        const res = await fetch(`${API_URL}/${this.props.id}?_embed`)
-        const json = await res.json()
-        const blogPost = {
-            date: new Date(json.date).toLocaleDateString(),
-            title: json.title.rendered,
-            content: json.content.rendered,
-            mediaURL: json._embedded['wp:featuredmedia'][0].source_url
-        }
-        this.setState({ post: blogPost })
-    }
-    render() {
-        return (
-            <section id='blog-post-container'>
-                {!this.state || !this.state.post ?
-                        <div className="loading">
-                            <div className="spinner-donut large tertiary"></div>
-                        </div>
-                        :
-                <div className='blog-card-container animated fadeIn'>
-                    <div className="blog-card-img">
-                        <img src={this.state.post.mediaURL} alt={this.state.post.title}/>
+const BlogPost = props => {
+    const { post } = props
+    return (
+        <section id='blog-post-container'>
+            {!props || !post ?
+                    <div className="loading">
+                        <div className="spinner-donut large tertiary"></div>
                     </div>
-                    <p className="blog-date">{this.state.post.date}</p>
-                    <h3>{this.state.post.title}</h3>
-                    <p className="blog-content" dangerouslySetInnerHTML={{__html: this.state.post.content}}></p>
+                    :
+            <div className='blog-card-container animated fadeIn'>
+                <div className="blog-card-img">
+                    <img src={post.mediaURL} alt={post.title}/>
                 </div>
-                }
-            </section>
-        );
-    }
+                <p className="blog-date">{post.date}</p>
+                <h3>{post.title}</h3>
+                <p className="blog-content" dangerouslySetInnerHTML={{__html: post.content}}></p>
+            </div>
+            }
+        </section>
+    );
+};
+
+BlogPost.propTypes = {
+    post: PropTypes.object.isRequired
 };
 
 export default BlogPost;
-
 
